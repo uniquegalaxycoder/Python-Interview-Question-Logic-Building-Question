0f834116fca7930c27b4df6b9b2da2a5bce56286
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2026-01-17 23:39:46 +0530

    Create 05 Calculate and print the sum of the first n natural numbers.py

---
+++ 05 Calculate and print the sum of the first n natural numbers.py
@@ -0,0 +1,10 @@
+# Calculate and print the sum of the first n natural numbers. 
+ Hint : (n * (n+1)) / 2 ( formula to find the n sum ) 
+
+start = 1 
+end = 10
+
+while start <= end :
+    d = (start*(start+1))/2 
+    start += 1
+print(d)
